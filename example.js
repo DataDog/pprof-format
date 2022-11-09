8b74e3f3e640831a807e0ea41aa3d23ae2543846
Stephen Belanger <stephen.belanger@datadoghq.com>
2022-11-09 09:43:40 -0800

    Make open-source-ready

--- example.js
+++ example.js
@@ -1,3 +1,9 @@
+/**
+ * Unless explicitly stated otherwise all files in this repository are licensed under the MIT License.
+ * 
+ * This product includes software developed at Datadog (https://www.datadoghq.com/  Copyright 2022 Datadog, Inc.
+ */
+
 import {
   Function,
   Label,
